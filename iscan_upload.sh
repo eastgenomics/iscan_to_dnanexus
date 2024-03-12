a242bf0099c8c759f37c28b176f7fedfcb9cc641
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2024-03-12 17:39:54 +0000

    replaced hardcoding with config parsing

--- iscan_upload.sh
+++ iscan_upload.sh
@@ -1,47 +1,26 @@
-auth_token=$(cat /mnt/storage/data/DNAnexus/iScan/api_token)
+#!/bin/bash
 
-# iScan data to be uploaded is stored locally within this dir
-iscan_dir="/mnt/storage/samba/samba.ctrulab.uk/cytogenetics/iScan"
+typeset -A CONFIG
+CONFIG=(
+    [AUTH_TOKEN_PATH]=""
+    [ISCAN_DIR]=""
+    [UPLOAD_AGENT_PATH]=""
+    [DX_PROJECT_ID]=""
+)
 
-# Upload agent executable is stored locally in this dir
-ua_path="/mnt/storage/apps/software/dnanexus_ua/1.5.31"
+while read LINE;
+do
+    if echo $LINE | grep -F = &>/dev/null
+    then
+        VARNAME=$(echo "$LINE" | cut -d '=' -f 1)
+        CONFIG[$VARNAME]=$(echo "$LINE" | cut -d '=' -f 2-)
+    fi
+done < iscan_upload.conf
 
-
-# Upload data in GS_reports dir
-reports_command="${ua_path}/ua --auth-token $auth_token --recursive --project=project-G2Y0BFj4xX2f1kKp5QbP7VJX $iscan_dir/GS_reports"
-
-echo $reports_command
-eval $reports_command
-
-reports_exit_code=$?
-
-
-# Upload data in GS_projects dir
-projects_command="${ua_path}/ua --auth-token $auth_token --recursive --project=project-G2Y0BFj4xX2f1kKp5QbP7VJX $iscan_dir/GS_projects"
-
-echo $projects_command
-eval $projects_command
-
-projects_exit_code=$?
-
-# Output results summary
-printf '\n\n\n\n##### RESULT #####\n\n'
-
-if [ $reports_exit_code -ne 0 ]; then
-    echo "Upload ERROR. A problem was encountered when uploading GS_reports/. Error code: ${reports_exit_code}"
-fi
-
-if [ $projects_exit_code -ne 0 ]; then
-    echo "Upload ERROR. A problem was encountered when uploading GS_projects/. Error code: ${projects_exit_code}"
-fi
-
-if [ $reports_exit_code -eq 0 ] && [ $projects_exit_code -eq 0 ]; then
-    echo "Upload SUCCESSFUL"; 
-fi
-
-printf '\n##################\n\n'
-
-if [ $reports_exit_code -ne 0 ] || [ $projects_exit_code -ne 0 ]; then
-    exit 1
-fi
-
+for DATA_SUBDIR in GS_reports GS_projects; do
+    ${CONFIG[UPLOAD_AGENT_PATH]}/ua \
+        --auth-token $(cat ${CONFIG[AUTH_TOKEN_PATH]}) \
+        --recursive \
+        --project=${CONFIG[DX_PROJECT_ID]} \
+        ${CONFIG[ISCAN_DIR]}/${DATA_SUBDIR}
+done
