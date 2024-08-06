1513967070c422173aa0812ec40b14919a6aea9a
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2024-08-06 14:44:35 +0100

    added error handling and extended commentary

--- iscan_upload.sh
+++ iscan_upload.sh
@@ -18,6 +18,16 @@ do
     fi
 done < $ISCAN_CONFIG_PATH 
 
+
+# The upload agent (ua) uploads all files it can find with a recursive search,
+# which is why we point it at subdirectories instead of individual files.
+#
+# The search is restricted to sites with "GS_" as a prefix, so we don't 
+# accidentally upload non-iScan data to DNANexus.
+#
+# If the upload agent (ua) throws an error, it'll do so by returning a non-zero
+# exit code. We don't want to archive any data that didn't get uploaded for
+# whatever reason, so we terminate the process if an error is thrown.
 for DATA_SUBDIR in ${CONFIG[ISCAN_DIR]}/GS_*; do
     SUBDIR_NAME=$(basename $DATA_SUBDIR)
     ${CONFIG[UPLOAD_AGENT_PATH]}/ua \
@@ -25,13 +35,19 @@ for DATA_SUBDIR in ${CONFIG[ISCAN_DIR]}/GS_*; do
         --recursive \
         --project=${CONFIG[DX_PROJECT_ID]} \
         ${CONFIG[ISCAN_DIR]}/${SUBDIR_NAME}
+    if [ $? != 0]; then
+        echo "ERROR - SEE LOGS"
+        exit 1
+    fi
 done
 
-# move all to archive
+# When the upload is confirmed, all data is moved to an archive directory.
+# There is no actual process for archiving yet, so all we do is move
+# files from the upload directories to an uncompressed "archive" directory
+# somewhere else on the system. This is defined in the config.
 echo "Archiving data..."
 for DATA_SUBDIR in ${CONFIG[ISCAN_DIR]}/GS_*; do
     SUBDIR_NAME=$(basename $DATA_SUBDIR)
-    # make it if it doesn't exist
     mkdir -p ${CONFIG[ARCHIVE_DIR]}/${SUBDIR_NAME}
     mv ${CONFIG[ISCAN_DIR]}/${SUBDIR_NAME}/* ${CONFIG[ARCHIVE_DIR]}/${SUBDIR_NAME}/
 done
